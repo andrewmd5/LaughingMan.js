594e2bf7986273fb7c4a2f1cfb67e1c00a585cc7
Andrew Sampson <andrew@codeusa523.org>
2015-12-07 16:44:53 -0500

    Create laughingman.js

---
+++ laughingman.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Andrew on 12/7/2015.
+ */
+function LaughingMan() {
+
+    this.laughingManImage = "https://i.imgur.com/u6A3oCM.gif";
+}
+
+LaughingMan.prototype.start = function() {
+    if (!document.getElementsByTagName) return;
+    var imageArray = document.getElementsByTagName("img");
+    var content = new Array();
+    var zImages;
+    var activeImages = 0;
+    for (var i = 0; i < imageArray.length; i++) {
+        content[activeImages] = imageArray[i];
+        activeImages++;
+    }
+    for (var x = 0, y = imageArray.length; x < y; x++) {
+        if (content[x]) {
+            content[x].style.background = "url(" + content[x].src + ")";
+            if (!content.push || !document.all) {
+                content[x].height = imageArray[x].height;
+                content[x].width = imageArray[x].width;
+                content[x].src = "https://i.imgur.com/u6A3oCM.gif";
+            } else {
+                content[x].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',src='img/new_png.png')";
+                content[x].height = imageArray[x].height;
+                content[x].width = imageArray[x].width;
+                content[x].src = this.laughingManImage;
+
+            }
+        }
+    }
+};
+
+var laughingman = new LaughingMan();
